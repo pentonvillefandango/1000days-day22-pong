af49beff3915f0f57285cd7b9e04f408bf5d484b
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2022-12-05 16:23:21 +0000

    Finished

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,29 +10,41 @@ screen.bgcolor("black")
 screen.title("PONG")
 screen.tracer(0)
 screen.listen()
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = ScoreBoard()
+speed = 0.1
 
-screen.onkey(r_paddle.paddle_up,"Up")
-screen.onkey(r_paddle.paddle_down,"Down")
-screen.onkey(l_paddle.paddle_up,"w")
-screen.onkey(l_paddle.paddle_down,"s")
+screen.onkey(r_paddle.paddle_up, "Up")
+screen.onkey(r_paddle.paddle_down, "Down")
+screen.onkey(l_paddle.paddle_up, "w")
+screen.onkey(l_paddle.paddle_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
+    ball.move_ball()
     screen.update()
 
+    # Detect top or bottom wall collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
+    # Detect ball out of bounds, reset and increment score
+    if ball.xcor() > 400:
+        ball.serve()
+        score.l_point()
 
 
-
-
-
-
-
-
-
+    if ball.xcor() < -400:
+        ball.serve()
+        score.r_point()
 
 
 screen.exitonclick()
